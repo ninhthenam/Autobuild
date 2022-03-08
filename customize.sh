d3d0887a6433f5dd6d1296d7629e5e7b68e231d6
390014043 <69024415+390014043@users.noreply.github.com>
2022-03-09 00:39:05 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,3 +6,16 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
+
+# 命令
+# echo "修改机器名称"
+# sed -i "s/OpenWrt/$device_name/g" package/base-files/files/bin/config_generate
+
+# echo "修改wifi名称"
+# sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# echo "修改时区"
+# sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
+
+# echo "修改默认主题"
+# sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
