ead1084d70df96fe63d89c1ebc44ddcb89d1bc2b
390014043 <69024415+390014043@users.noreply.github.com>
2022-03-09 11:08:00 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -20,7 +20,7 @@ echo "修改wifi名称"
 sed -i "s/OpenWrt/$wifi_name/g" openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 echo "修改时区"
-sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
+sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" openwrt/package/base-files/files/bin/config_generate
 
 # echo "修改默认主题"
 # sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
