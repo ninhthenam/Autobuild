ffffd60957e66ecbcf34210a7c4d088460380133
390014043 <69024415+390014043@users.noreply.github.com>
2022-03-10 16:08:08 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -11,3 +11,7 @@ sed -i "s/OpenWrt/$device_name/g" openwrt/package/base-files/files/bin/config_ge
 wifi_name=$device_name
 echo "修改wifi名称"
 sed -i "s/OpenWrt/$wifi_name/g" openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+default_theme='argon'
+echo "修改默认主题"
+sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
