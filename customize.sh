33e2d48e86449f7abb2b3c5ff07bd9d15ba6fcce
390014043 <69024415+390014043@users.noreply.github.com>
2022-03-11 20:20:13 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,14 +1,14 @@
-echo "更改默认IP，测试可用"
+echo "更改默认IP"
 sed -i 's/192.168.1.1/10.0.1.1/g' openwrt/package/base-files/files/bin/config_generate
 
-echo "清楚登陆密码，测试可用"
+echo "清楚登陆密码"
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-device_name='H-Wrt，测试可用'
+device_name='H-Wrt'
 echo "修改机器名称"
 sed -i "s/OpenWrt/$device_name/g" openwrt/package/base-files/files/bin/config_generate
 
 wifi_name=$device_name
-echo "修改wifi名称，未测试"
+echo "修改wifi名称"
 sed -i "s/OpenWrt/$wifi_name/g" openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
