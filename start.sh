b9b3dd4fa9e4af9497405979db1a6a081ba3e5dd
390014043 <69024415+390014043@users.noreply.github.com>
2022-07-12 15:57:14 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,14 @@
+git="https://github.com/"
+
+echo "开始安装git"
 apt update && apt install git
+echo "拉取源码"
+git clone ${git}coolsnowwolf/lede.git ~/lede
+git clone ${git}jerrykuku/luci-theme-argon ~/lede/package/lean/luci-theme-argon-18.06
+git clone ${git}vernesong/OpenClash ~/lede/package/OpenClash
+git clone ${git}rufengsuixing/luci-app-adguardhome ~/lede/package/luci-app-adguardhome
+echo "更新lede"
+cd ~/lede
+./scripts/feeds update -a
+./scripts/feeds install -a
+./scripts/feeds install -a
