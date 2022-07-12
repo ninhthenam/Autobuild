2e132dc7ead4b06f80f90bb7c90d083d79841cc4
390014043 <69024415+390014043@users.noreply.github.com>
2022-07-12 17:07:53 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,7 @@ git="https://mirror.ghproxy.com/https://github.com/"
 echo "开始安装git"
 sudo apt update && sudo apt install git vim -y
 echo "拉取源码"
-git clone ${git}coolsnowwolf/lede.git ~/lede
-git clone ${git}jerrykuku/luci-theme-argon ~/lede/package/lean/luci-theme-argon-18.06
-git clone ${git}vernesong/OpenClash ~/lede/package/OpenClash
-git clone ${git}rufengsuixing/luci-app-adguardhome ~/lede/package/luci-app-adguardhome
+git clone ${git}coolsnowwolf/lede.git ~/lede && git clone ${git}jerrykuku/luci-theme-argon ~/lede/package/lean/luci-theme-argon-18.06 && git clone ${git}vernesong/OpenClash ~/lede/package/OpenClash && git clone ${git}rufengsuixing/luci-app-adguardhome ~/lede/package/luci-app-adguardhome
 echo "更新lede"
 cd ~/lede
 ./scripts/feeds update -a
